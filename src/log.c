9aa4cde6220cffcd19720ffe30088f9465579e9f
eugeneandrienko <evg.andrienko@gmail.com>
2024-01-13 17:38:33 +0300

    Print time in console log

--- src/log.c
+++ src/log.c
@@ -1,9 +1,12 @@
 #include <stdarg.h>
 #include <stdio.h>
-
+#include <errno.h>
+#include <string.h>
+#include <time.h>
 #include "config.h"
 #include "log.h"
 
+#define LOG_BUFFER_SIZE 200
 
 static int foreground = 0;
 
@@ -24,7 +27,9 @@ void log_write(int priority, const char * format, ...)
 	if(foreground)
 	{
 		char * priorityStr = "";
-		char * messageStr = "";
+		char timeStr[LOG_BUFFER_SIZE];
+		char messageStr[LOG_BUFFER_SIZE];
+		time_t currentTime;
 
 		switch(priority)
 		{
@@ -56,14 +61,24 @@ void log_write(int priority, const char * format, ...)
 			priorityStr = "UNKNOWN PRIORITY";
 		}
 
-		// TODO: time instead of name:
-		fprintf(stderr, "%s [%s]: ", PACKAGE_NAME, priorityStr);
-		sprintf(messageStr, format, vlist);
+		time(&currentTime);
+		if(currentTime == ((time_t) -1))
+		{
+			sprintf(messageStr, "UNKNOWN TIME");
+			fprintf(stderr, "%s: Cannot get current time: %s", PACKAGE_NAME, strerror(errno));
+		}
+		else
+		{
+			sprintf(timeStr, "%s", ctime(&currentTime));
+			timeStr[strlen(timeStr) - 1] = '\0';
+		}
+		fprintf(stderr, "%s [%s]: ", timeStr, priorityStr);
+		vsprintf(messageStr, format, vlist);
 		fprintf(stderr, "%s\n", messageStr);
 	}
 	else
 	{
-		syslog(priority, format, vlist);
+		vsyslog(priority, format, vlist);
 	}
 
 	va_end(vlist);
